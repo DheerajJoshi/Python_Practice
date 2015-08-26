cf7b00e8379ff0251de3683050d169218485df7f
Dheeraj2017 <joshidj.12@gmail.com>
2015-08-26 16:04:03 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+print "this is introduction to python"
+print ("this is dheeraj kumar joshi")
+print("""introduction to the core python
+	and we enjoying in the class ClassName(object):
+		docstg for ClassName
+		def _it__(self, arg):
+			super(ClassName, self).__init__()
+			self.arg = arg
+			""")
